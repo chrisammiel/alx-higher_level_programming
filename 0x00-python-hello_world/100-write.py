7f5fb32fd0bdf4284d7e5b5fc437586d73309a2f
Chris <ishimwechrisamm@gmail.com>
2022-06-03 11:45:20 +0300

    python 6

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
-if
+#!/usr/bin/python3 
+import sys
 f = open("chris2.txt", "a" )
 f.write("\nand that piece of art is useful - Dora Korpar, 2015-10-19")
 f.close()
 
 f = open("chris2.txt", "r")
 print(f.read())
-os.write(1, b"spam\n")
+sys.stderr.write("spam\n")
+os.write(2, b"spam\n")

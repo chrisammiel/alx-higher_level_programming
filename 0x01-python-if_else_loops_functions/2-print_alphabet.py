8d981981534a13c4bd090855434d1f59d4303f13
Chris <ishimwechrisamm@gmail.com>
2022-06-06 14:50:56 +0300

    ascii_lowercases

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-alph = 97
-for i in range(97, 122):
-    print(chr(i), end="")
-    alph += 1
+for i in range(ord('a'), ord('z') + 1):
+    print(f"{chr(i)}", end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-alph = 97
-for i in range(97, 122):
-    if i == 101 and i == 112:
-        continue
-    print(chr(i), end="")
-    alph += 1
+for i in range(ord('a'), ord('z') + 1):
+    print(f"{chr(i)}", end="")

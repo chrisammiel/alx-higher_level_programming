9619c29e20691750f469d03bedeb690648df1ae8
Chris <ishimwechrisamm@gmail.com>
2022-06-06 14:18:37 +0300

    ascii_lowercases

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import string
+for letter in string.ascii_lowercase:
+    if letter in string.ascii_lowercase == e:
+        continue
+        print("{}".format(letter in string.ascii_lowercase), end="")

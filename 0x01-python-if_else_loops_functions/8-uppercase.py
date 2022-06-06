10c0083823688431b011ee3f3427df6e62d7b201
Chris <ishimwechrisamm@gmail.com>
2022-06-06 20:59:13 +0300

    ascii_lowercases

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in str[100]:
-        if ord('a') <= ord(i) <= ord('z'):
-            i = chr(ord(i) - 32)
-            print("{}".format(i), end="")
-        print("")
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            c = chr(ord(c) - (ord('a') - ord('A')))
+        print("{:s}".format(c), end='')
+    print("")

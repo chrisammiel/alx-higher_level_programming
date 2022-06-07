e0eb96cf4b0cdea928a81259eec45148d2b4724c
Chris <ishimwechrisamm@gmail.com>
2022-06-07 21:21:38 +0300

    advanced

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,15 +7,16 @@ if __name__ == "__main__":
     if len(sys.args) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
+    if op == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif op == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
-        if op == "+":
-            print("{} + {} = {}".format(a, b, add(a, b)))
-        elif op == "-":
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif op == "*":
-            print("{} * {} = {}".format(a, b, mul(a, b)))
-        elif op == "/":
-            print("{} / {} = {}".format(a, b, div(a, b)))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+

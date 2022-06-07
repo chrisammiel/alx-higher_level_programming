02c6445f3b12ec7c3bbb3c9de0f60efbb5869d5b
Chris <ishimwechrisamm@gmail.com>
2022-06-07 19:52:50 +0300

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,8 +8,6 @@ if __name__ == "__main__":
         print("{} argument:".format(len(sys.argv)-1))
     else:
         print("{} arguments:".format(len(sys.argv)-1))
-        l = len(argv)
     for i, s in enumerate(argv):
         if i > 0:
             print("{:d}: {:s}".format(i, s))
-

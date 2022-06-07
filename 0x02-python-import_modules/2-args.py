cc69980d0a3bfb2328bfaba26e6a1ce25d31f1d2
Chris <ishimwechrisamm@gmail.com>
2022-06-07 19:13:11 +0300

    addmodule

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if (len(sys.argv)-1) == 1:
+        print("{} argument:".format(len(sys.argv)-1))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    num_of_args = len(sys.argv[1:])
+    i = 1
+    while num_of_args >= 1:
+        print("{}: {}".format(i, argv[i]))
+        i += 1

33097838e4ecfb20dd31ad8895fed29e079b6caf
Chris <ishimwechrisamm@gmail.com>
2022-06-07 18:19:31 +0300

    addmodule

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
-a = int(1)
-b = int(2)
-print(f'{a} + {b} = {add(a, b)}')
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print(f'{a} + {b} = {add(a, b)}\n')

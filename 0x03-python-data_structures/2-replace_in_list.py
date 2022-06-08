907922c8d565914ab8a5121a5974cea448c4f3e3
Chris <ishimwechrisamm@gmail.com>
2022-06-08 22:42:52 +0300

    0x03

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    my_list[idx] = element
+def replace_in_list(my_list, idx, new_element):
+    my_list[idx] = new_element
     return my_list

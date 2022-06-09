c7503b607b4214df1e380088d2135c8f5224136c
Chris <ishimwechrisamm@gmail.com>
2022-06-09 21:40:56 +0300

    div

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,10 +1,5 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 import sys
-f = open("chris2.txt", "a")
-f.write("\nand that piece of art is useful - Dora Korpar, 2015-10-19")
-f.close()
 
-f = open("chris2.txt", "r")
-print(f.read())
-sys.stderr.write("spam\n")
-os.write(2, b"spam\n")
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+exit(1)
